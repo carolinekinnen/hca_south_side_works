b15356410e4dbcc6905bbbd02048249049f1214c
Caroline Kinnen <ckinnen@uchicago.edu>
2021-01-20 23:55:35 -0500

    set up repo and Project Template

---
+++ data/census_data.R
@@ -0,0 +1,12 @@
+
+
+library(tidycensus)
+
+# need to register API key to use^
+census_api_key("36f9784d961adc74d5268b0733587a8a3989d390", install = TRUE)
+
+# neighborhood (nb) #s below poverty level
+# nb #s received public or cash assist
+# nb #s no access to internet
+# nb #s under 18 (all age groups)
+# nb #s grocery stores, pharmacies, banks, currency exchange per 100,000
